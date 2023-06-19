88f888616b95e6359d664c9573e3abdb0e57ad4f
svetomsk <svt2404@gmail.com>
2023-06-19 19:01:53 +0600

    Task 20

---
+++ src/ValidParantheses_20.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ValidParantheses_20 {
+    class Solution {
+        public boolean isValid(String s) {
+            LinkedList<Character> q = new LinkedList<>();
+            q.add(s.charAt(0));
+            Set<Character> opening = new HashSet<>() {{
+                add('(');
+                add('{');
+                add('[');
+            }};
+            Map<Character, Character> pairs = new HashMap<>() {{
+                put(')', '(');
+                put(']', '[');
+                put('}', '{');
+            }};
+            for (int i = 1; i < s.length(); i++) {
+                Character current = s.charAt(i);
+                if (opening.contains(current)) {
+                    q.add(current);
+                } else {
+                    if (q.size() == 0) return false;
+                    Character last = q.pollLast();
+                    if (last != pairs.get(current)) return false;
+                }
+            }
+            return q.size() == 0;
+        }
+    }
+}
